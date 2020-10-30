7e97276074c0d34279db2cbb3803927bb0fa3823
Isaac Santiago <62231878+isaacsantiago24@users.noreply.github.com>
2020-10-29 20:51:42 -0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom'; //to use the render function
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <App /> 
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // root is the div inside of the public 
 );
 
 // If you want to start measuring performance in your app, pass a function
